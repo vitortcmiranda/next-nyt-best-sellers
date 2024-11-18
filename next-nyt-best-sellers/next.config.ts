b70784455b577179f6fa22f03e101d4971555cd6
vitor.miranda <vitor.miranda@ifood.com.br>
2024-11-18 08:59:27 -0300

    feat: creating books by category page

--- next-nyt-best-sellers/next.config.ts
+++ next-nyt-best-sellers/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {protocol: 'https', hostname: 'storage.googleapis.com'},
+    ]
+  }
 };
 
 export default nextConfig;
--- next-nyt-best-sellers/src/app/aggregates/nytimes-api/nytimes-api-response.ts
+++ next-nyt-best-sellers/src/app/aggregates/nytimes-api/nytimes-api-response.ts
@@ -5,4 +5,28 @@ export default interface BestsellerCategoriesResponse{
     oldest_published_date: string;
     newest_published_date: string;
     updated: string;
+    books?: Book[];
+}
+
+interface Book {
+    rank: number;
+    rank_last_week: number;
+    weeks_on_list: number;
+    asterisk: number;
+    dagger: number;
+    publisher: string;
+    description: string;
+    title: string;
+    author: string;
+    contributor: string;
+    book_image: string;
+    book_image_width: number;
+    book_image_height: number;
+    amazon_product_url: string;
+    buy_links: BuyLink[];
+}
+
+interface BuyLink {
+    name: string;
+    url: string;
 }
